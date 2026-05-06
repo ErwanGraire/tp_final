310975905e1e3aab6e5c475a819291ee2975a1f6
Errwaannn <144239049+ErwanGraire@users.noreply.github.com>
2026-05-06 20:14:35 +0200

    fix: script get_ip pour environnement local

--- get_ip.sh
+++ get_ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Récupère l'IP de la VM et nettoie les caractères Windows
-IP=$(vagrant ssh -c "hostname -I | awk '{print \$1}'" 2>/dev/null | tr -d '\r')
+# Récupère l'IP interne de la VM Debian directement
+IP=$(hostname -I | awk '{print $1}')
 
 if [ -z "$IP" ]; then
     exit 1
