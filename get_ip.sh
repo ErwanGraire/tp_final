98b14f5d5556bda1a8c66eea8aacc1aa4aa5e74f
Errwaannn <144239049+ErwanGraire@users.noreply.github.com>
2026-05-07 02:38:12 +0200

    Dépôt final : README complet, scripts et monitoring opérationnels

--- get_ip.sh
+++ get_ip.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# Récupère l'IP interne de la VM Debian directement
-IP=$(hostname -I | awk '{print $1}')
+# Ce script doit être exécuté sur la VM Debian
+# Il récupère l'IP de l'interface eth1 (réseau privé Vagrant)
+
+IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 if [ -z "$IP" ]; then
+    echo "Erreur : Impossible de trouver l'IP sur eth1."
     exit 1
 else
     echo "$IP"
