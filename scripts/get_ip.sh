e48cd4b4546269a7363c7d83e2e324ab180bd700
Raffael2225 <raffaeltano27@gmail.com>
2026-05-07 11:45:02 +0200

    Refactor get_ip.sh by removing comments
    Removed comments in French and cleaned up the script.

--- scripts/get_ip.sh
+++ scripts/get_ip.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# Script de récupération d'IP via DHCP pour inventaire Ansible 
 
-# On récupère l'IP sur l'interface eth1 (standard sur VirtualBox/Vagrant)
-# On utilise vagrant ssh pour exécuter la commande dans la VM 
 VM_IP=$(cd vagrant && vagrant ssh -c "ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'" | tr -d '\r')
 
 if [ -z "$VM_IP" ]; then
@@ -12,7 +9,7 @@ fi
 
 echo "IP détectée : $VM_IP"
 
-# Création automatisée de l'inventaire Ansible 
+
 cat <<EOT > ansible/inventory.ini
 [k3s_nodes]
 k3s-server ansible_host=$VM_IP ansible_user=vagrant ansible_ssh_private_key_file=vagrant/.vagrant/machines/default/virtualbox/private_key
